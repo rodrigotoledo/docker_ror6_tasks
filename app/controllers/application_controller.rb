3018ee6cfc9e932b3a5c6db417236a434ee5391a
Rodrigo Toledo <rodrigotoledo@MacBook-RToledo-dev-6.local>
2021-12-05 11:24:41 -0300

    #stimulus reflex - stimulus reflex gem - search trigger after change something in text tag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 #:nocov:
 class ApplicationController < ActionController::Base
 before_action :authenticate_user!
+before_action :set_action_cable_identifier
+
+  private
+
+  def set_action_cable_identifier
+    cookies.encrypted[:user_id] = current_user&.id
+  end
 end
 #:nocov:
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.new(email: 'test@test.com', first_name: Faker::Name.female_first_name, last_name: '...', password: 'aassdd', password_confirmation: 'aassdd')
+user.save
+
+100.times do
+  Author.create(name: Faker::Name.name_with_middle, email: Faker::Internet.email)
+end
